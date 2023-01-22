3c4af9db2b32234fd3a548e15ad9fd88ec0bb6ee
theshy0404 <1318936142@qq.com>
2023-01-22 20:52:01 +0800

    feat: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Space, Result } from 'antd';
-import axios from 'axios';
-import LOADING_GIF from './public/loading.gif';
-import './App.css';
+import React from "react";
+import Home from "./pages/home";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
-  const [msg, setMsg] = useState('');
-  const [count, setCount] = useState(0);
-  const [isLoading, setLoading] = useState(true);
-  const [isError, setError] = useState(false);
-
-  useEffect(() => {
-    setTimeout(fetchData, 2000)
-  }, []);
-
-  const fetchData = () => {
-    axios('http://127.0.0.1:5000/', {
-      method: 'GET', // or 'POST'
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(res => {
-        console.log(res)
-        setMsg(res.data);
-      })
-      .catch((error) => {
-        console.log(error)
-        setError(true)
-      })
-      .finally(() => {
-        setLoading(false)
-      });
-  }
-
   return (
-    <>
-      {
-        isLoading ? <img src={LOADING_GIF} /> : (
-          isError ? <Result
-            status="error"
-            title="There are some problems..."
-            extra={
-              <Button type="primary" key="again">
-                Try it again
-              </Button>
-            }
-          /> : (
-            <>
-              <h1>{msg}</h1>
-              <h2>count: {count}</h2>
-              <Space>
-                <Button onClick={() => setCount(count + 1)}>add</Button>
-                <Button onClick={() => setCount(count - 1)}>reduce</Button>
-              </Space>
-            </>)
-        )
-      }
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/error" element={<h1>404</h1>} />
+      </Routes>
+    </HashRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { Button, Space, Result } from "antd";
+import axios from "axios";
+import LOADING_GIF from "../../public/loading.gif";
+import styles from "./styles.module.css";
+
+const Home = () => {
+  const [msg, setMsg] = useState("");
+  const [count, setCount] = useState(0);
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
+
+  useEffect(() => {
+    setTimeout(fetchData, 2000);
+  }, []);
+
+  const fetchData = () => {
+    axios("http://127.0.0.1:5000/", {
+      method: "GET", // or 'POST'
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        setMsg(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <img src={LOADING_GIF} />
+      ) : isError ? (
+        <Result
+          status="error"
+          title="There are some problems..."
+          extra={
+            <Button type="primary" key="again">
+              Try it again
+            </Button>
+          }
+        />
+      ) : (
+        <div className={styles.wrap}>
+          <h1>{msg}</h1>
+          <h2>count: {count}</h2>
+          <Space>
+            <Button onClick={() => setCount(count + 1)}>add</Button>
+            <Button onClick={() => setCount(count - 1)}>reduce</Button>
+          </Space>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Home;
