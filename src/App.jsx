db3bd3d6a43bca8aa8d7fd67390901de26300761
theshy0404 <1318936142@qq.com>
2023-01-23 01:00:31 +0800

    feat: router

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
     <HashRouter>
       <Routes>
         <Route index path="/" element={<Home />} />
+        <Route path="/admin">
+          <Route path="/admin/" element={<h1>admin</h1>}></Route>
+          <Route path="/admin/user" element={<h1>user</h1>}></Route>
+          <Route path="/admin/system" element={<h1>system</h1>}></Route>
+        </Route>
         <Route path="/error" element={<h1>404</h1>} />
       </Routes>
     </HashRouter>
